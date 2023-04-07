ac6a40cb5b74aea619af9562b6270b31600ab136
Emelme <125494917+Emelme@users.noreply.github.com>
2023-04-07 18:39:52 +0200

    Re-create scripts

---
+++ Tree Evolution Unity Project/Assets/Scripts/Data/TreeData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeData : MonoBehaviour
+{
+	public int maxAge;
+	public int age;
+	public int mass;
+	public int height;
+	public int width;
+	public int energy;
+	public int needEnergy;
+}
