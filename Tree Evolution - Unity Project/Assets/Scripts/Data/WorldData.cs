ded1ee0df49f86d91331423ada12c5e91b97ac6c
Emelme <125494917+Emelme@users.noreply.github.com>
2023-04-07 17:55:14 +0200

    Create a lot of Scripts
    RandomTreeColor, Data Scripts, Genes Scripts

---
+++ Tree Evolution - Unity Project/Assets/Scripts/Data/WorldData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldData : MonoBehaviour
+{
+	public int sunPower;
+	public int sunForce;
+}
---
+++ Tree Evolution - Unity Project/Assets/Scripts/Genes/Genome.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList;
+using UnityEngine;
+
+public class Genome : MonoBehaviour
+{
+	public Gene[] genome = new Gene[16];
+
+	private void Start()
+	{
+		for (int i = 0; i < genome.Length; i++)
+		{
+			genome[i] = new Gene();
+		}
+	}
+
+	public void RandomValueInGene()
+	{
+		for (int i = 0; i < genome.Length; i++)
+		{
+			genome[i].number = ++i;
+
+			if (Random.Range(0, 3) == 0) genome[i].leftGene = Random.Range(1, 17);
+			if (Random.Range(0, 3) == 0) genome[i].leftUpGene = Random.Range(1, 17);
+			if (Random.Range(0, 3) == 0) genome[i].rightUpGene = Random.Range(1, 17);
+			if (Random.Range(0, 3) == 0) genome[i].rightGene = Random.Range(1, 17);
+			if (Random.Range(0, 3) == 0) genome[i].rightDownGene = Random.Range(1, 17);
+			if (Random.Range(0, 3) == 0) genome[i].leftDownGene = Random.Range(1, 17);
+
+			genome[i].parameter1 = 
+
+			if (Random.Range(0, 4) == 0)
+			{
+			}
+		}
+	}
+}
